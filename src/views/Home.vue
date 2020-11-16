53fff065ac7f069d246b16502d8b2006777b5edf
kim <eltmzjqjfl007@gmail.com>
2020-11-17 01:28:49 +0900

    수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,6 +61,23 @@ export default {
       // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
       var zoomControl = new kakao.maps.ZoomControl();
       map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+      kakao.maps.event.addListener(map, "rightclick", function (mouseEvent) {
+        var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+         iwPosition = mouseEvent.latLng; // new kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
+        var iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+        // 인포윈도우를 생성하고 지도에 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+          map: map, // 인포윈도우가 표시될 지도
+          position: iwPosition,
+          content: iwContent,
+          removable: iwRemoveable,
+        });
+       
+        //infowindow.close();   
+        // console.log("rightClick", mouseEvent);
+      });
     },
   },
 };
